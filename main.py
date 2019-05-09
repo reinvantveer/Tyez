a6cce41c1e0ce42d47b81795ab285a976efedc8a
Rein van 't Veer <reinvantveer@gmail.com>
2019-05-09 21:22:34 +0200

    parse com port from program args

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+import time
+import argparse
+
+
 from pyfirmata import Arduino
+from serial import SerialException
+
+parser = argparse.ArgumentParser(
+  description='REST API and web interface to Arduino analog port read',
+  formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+
+parser.add_argument('-p', '--port', required=True, help='COM port to communicate with the Arduino')
+args = parser.parse_args()
+
+try:
+  board = Arduino(args.port)
+except SerialException as e:
+  print('Unable to connect Arduino on port {}'.format(args.port))
 
 print('Done!')
