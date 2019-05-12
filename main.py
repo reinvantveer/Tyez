25781ea9095c585cd1fdd87b347a40c857623aa4
Rein van 't Veer <reinvantveer@gmail.com>
2019-05-12 14:52:04 +0200

    indentation

--- main.py
+++ main.py
@@ -8,30 +8,30 @@ from serial_scan.scan import get_com_ports
 from util.yes_no import query_yes_no
 
 parser = argparse.ArgumentParser(
-  description='REST API and web interface to Arduino analog port read',
-  formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    description='REST API and web interface to Arduino analog port read',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
 )
 
 parser.add_argument('-p', '--port', required=True, help='COM port to communicate with the Arduino')
 args = parser.parse_args()
 
 try:
-  board = Arduino(args.port)
+    board = Arduino(args.port)
 except SerialException as e:
-  print('Unable to connect Arduino on port {}'.format(args.port))
-  com_ports = get_com_ports()
-  if len(com_ports) == 0:
-    print('No COM ports in use detected. Is the Arduino connected?')
-    sys.exit(1)
-  elif len(com_ports) == 1:
-    question = 'Found COM port ' + com_ports[0] + ', retry using this port?'
-    answer = query_yes_no(question)
-    if answer:
-      board = Arduino(com_ports[0])
+    print('Unable to connect Arduino on port {}'.format(args.port))
+    com_ports = get_com_ports()
+    if len(com_ports) == 0:
+        print('No COM ports in use detected. Is the Arduino connected?')
+        sys.exit(1)
+    elif len(com_ports) == 1:
+        question = 'Found COM port ' + com_ports[0] + ', retry using this port?'
+        answer = query_yes_no(question)
+        if answer:
+            board = Arduino(com_ports[0])
+        else:
+            print('Exiting')
+            sys.exit(1)
     else:
-      print('Exiting')
-      sys.exit(1)
-  else:
-    print('I do have COM ports', ', '.join(com_ports), 'you may want to try one of these.')
+        print('I do have COM ports', ', '.join(com_ports), 'you may want to try one of these.')
 
 print('Connected!')
